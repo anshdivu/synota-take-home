ae0571c6e77dee28cd7ffdafe5989949048ee21e
Diviyansh Bhatnagar <dbhatnagar@wayfair.com>
2023-01-15 00:42:50 -0500

    cleanup

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import todoRoutes from "./todo.routes";
 const app = express();
 
 app.disable("x-powered-by");
-app.use(morgan("combined"));
+app.use(morgan("short"));
 
 app.get("/liveness", (_, res) => res.send("Live"));
 app.get("/readiness", (_, res) => res.send("Ready"));
